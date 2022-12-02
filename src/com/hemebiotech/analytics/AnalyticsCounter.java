aa4e99d7796bad7e718d9c5610a7cc352e0e98c7
Ndeyea10 <ndeye.sylla@etud.univ-angers.fr>
2022-12-02 21:32:49 +0100

    Création de la méthode countSymptoms et appel de la méthode dans le main

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,31 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.List;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+	/**
+	 * allows to sort in alphabetical order and to count for each symptom the number of occurrences
+	 * @param symptoms list and creates a TreeMap to check for each symptom the number of occurrences
+	 * @return the list of different symptoms with their occurrence.
+	 */
+	public TreeMap<String,Integer> countSymptoms(List<String> symptoms) {
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+		TreeMap<String, Integer> listSymptoms = new TreeMap<>();
+		if (listSymptoms != null) {
+			for (String symptom : symptoms) {
+				if (!listSymptoms.containsKey(symptom)){
+					listSymptoms.put(symptom, 1);
+				}
+				else {
+					int value = listSymptoms.get(symptom);
+					value++;
+					listSymptoms.put(symptom, value);
+				}
 			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+
+		return listSymptoms;
+
 	}
 }
--- src/com/hemebiotech/analytics/Main.java
+++ src/com/hemebiotech/analytics/Main.java
@@ -2,6 +2,10 @@ package com.hemebiotech.analytics;
 
 public class Main {
     public static void main(String[] args) {
+        AnalyticsCounter counter = new AnalyticsCounter();
+        ISymptomReader read= new ReadSymptomDataFromFile("symptoms.txt");
+        System.out.println(read.getSymptoms());
+        System.out.println(counter.countSymptoms(read.getSymptoms()));
 
     }
 }
